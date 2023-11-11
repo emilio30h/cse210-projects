9e52676cb78c55767f49c6eb90ee01905f32271d
emilio30h <emilioherimanitra3@Dgmail.com>
2023-11-11 15:46:11 +0300

    Prep 2  and 3 done

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        /*Console.Write("What is the magic number? ");
+        string magic = Console.ReadLine();
+        int magic0 = int.Parse(magic);*/
+
+        Random randomGenerator = new Random();
+        int magic0 = randomGenerator.Next(1, 11);
+
+
+        Console.Write("What is your guess? ");
+        string guess = Console.ReadLine();
+        int guess0 = int.Parse(guess);
+        
+
+        while (guess0 != magic0)
+        {
+            if (guess0 < magic0)
+            {
+                Console.Write("Higher. ");
+            }
+
+            else if (guess0 > magic0)
+            {
+                Console.Write("Lower. ");
+            }
+
+            Console.Write("What is your guess? ");
+            guess = Console.ReadLine();
+            guess0 = int.Parse(guess);
+        }
+
+        Console.Write("You guessed it! ");
+        
     }
 }
