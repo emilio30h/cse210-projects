7e624bb52c88ab388a82cee7bde4059f08a4628d
emilio30h <emilioherimanitra3@Dgmail.com>
2023-11-12 16:28:03 +0300

    week 04 prep done

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,49 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        int UserInput = -1;
+
+        List<int> numbers = new List<int>();
+
+        while (UserInput != 0)
+        {
+            Console.WriteLine("Enter a list of numbers, type 0 when finished: ");
+            UserInput = int.Parse(Console.ReadLine());
+
+            if (UserInput != 0)
+            {
+                numbers.Add(UserInput);
+            }
+        }
+
+
+        //Calculating the sum
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine($"The sum is: {sum}. ");
+
+        // Calculating the average of the numbers
+        float average = (float)sum / (numbers.Count);
+        Console.WriteLine($"The average is: {average}. ");
+
+        // Finding the maximum number
+        int max = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max =  number;
+            }
+        }
+
+        Console.WriteLine($"The maximum is {max}");
     }
 }
