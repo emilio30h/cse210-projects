18be9a3d9f40d10e4baa14aa97b69a2d22c25465
emilio30h <emilioherimanitra3@Dgmail.com>
2023-12-01 11:29:37 +0300

    done w03 assignment

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        // Create an instance of the Scripture class
+        Scripture scripture = new Scripture(new Reference("John", 3, 16), "For God so loved the world, that he gave his only Son, that whoever believes in him should not perish but have eternal life.");
+
+        // Display the complete scripture
+        Console.WriteLine(scripture.GetDisplayText());
+
+        // Prompt the user to press enter or type quit
+        string input = Console.ReadLine();
+
+        while (input != "quit")
+        {
+            // Hide a few random words
+            scripture.HideRandomWords(3);
+
+            // Clear the console
+            ClearConsole();
+
+            // Display the updated scripture with hidden words
+            Console.WriteLine(scripture.GetDisplayText());
+
+            // Prompt the user again
+            input = Console.ReadLine();
+        }
+    }
+
+    static void ClearConsole()
+    {
+        Console.Clear();
+        Console.SetCursorPosition(0, 0);
     }
 }
---
+++ prove/Develop03/Scripture.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+
+class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words;
+
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+        _words = new List<Word>();
+
+        string[] wordArray = text.Split(' ');
+
+        foreach (string wordText in wordArray)
+        {
+            Word word = new Word(wordText);
+            _words.Add(word);
+        }
+    }
+
+    public void HideRandomWords(int numberToHide)
+    {
+        Random random = new Random();
+
+        for (int i = 0; i < numberToHide; i++)
+        {
+            int index = random.Next(_words.Count);
+            _words[index].Hide();
+        }
+    }
+
+    public string GetDisplayText()
+    {
+        string displayText = $"{_reference.GetDisplayText()}\n";
+
+        foreach (Word word in _words)
+        {
+            displayText += $"{word.GetDisplayText()} ";
+        }
+
+        return displayText.Trim();
+    }
+}
