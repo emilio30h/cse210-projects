1d2619183fa2ed445904998a98dc80080c9be5c3
emilio30h <emilioherimanitra3@Dgmail.com>
2023-12-12 18:07:24 +0300

    foundation 1

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -1,9 +1,38 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation1 World!");
+        // Create a video
+        Video video = new Video
+        {
+            Title = "Introduction to Programming",
+            Author = "John Doe",
+            Length = 120,
+            Comments = new List<Comment>()
+        };
+
+        // Add comments to the video
+        video.Comments.Add(new Comment { Name = "User1", Text = "Great video!" });
+        video.Comments.Add(new Comment { Name = "User2", Text = "Very informative." });
+        video.Comments.Add(new Comment { Name = "User3", Text = "I learned a lot." });
+
+        // Display video details and comment count
+        Console.WriteLine("Video Details:");
+        Console.WriteLine($"Title: {video.Title}");
+        Console.WriteLine($"Author: {video.Author}");
+        Console.WriteLine($"Length: {video.Length} minutes");
+        Console.WriteLine($"Comment count: {video.GetCommentCount()}");
+
+        // Display comments
+        Console.WriteLine("\nComments:");
+        foreach (Comment comment in video.Comments)
+        {
+            Console.WriteLine($"Name: {comment.Name}");
+            Console.WriteLine($"Text: {comment.Text}");
+            Console.WriteLine();
+        }
     }
 }
